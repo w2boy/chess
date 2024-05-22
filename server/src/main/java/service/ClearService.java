e59b402e62093415a613a701635a5cb4517392a9
w2boy <wesleyt.wardle@gmail.com>
2024-05-21 21:41:49 -0600

    Added Service Classes

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,9 @@
+package service;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+public class ClearService {
+    public void clear() { }
+   }
---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+public class GameService {
+    public GameData[] listGames(AuthData authData) {
+        return null;
+    }
+    public GameData createGame(GameData gameData, AuthData authData) {
+        return null;
+    }
+    public void joinGame(GameData gameData, AuthData authData) {}
+}
