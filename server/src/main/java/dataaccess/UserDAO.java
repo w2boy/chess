8d9f5496b0ddc8f496a03452baac433b855c9773
w2boy <wesleyt.wardle@gmail.com>
2024-06-01 19:37:45 -0600

    Fixed UserDAO to run quality code check

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -6,7 +6,7 @@ import javax.xml.crypto.Data;
 
 public interface UserDAO {
     void deleteAllUsers() throws DataAccessException;
-    void findUser() throws DataAccessException;
+    void userExists() throws DataAccessException;
     void createUser() throws DataAccessException;
     UserData getUser() throws DataAccessException;
 
