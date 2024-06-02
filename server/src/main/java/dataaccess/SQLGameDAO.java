21f15d6258208e12d1e08e9bd514f1daaef93ed7
w2boy <wesleyt.wardle@gmail.com>
2024-06-01 18:23:42 -0600

    Implemented list games phase 4

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -3,10 +3,13 @@ package dataaccess;
 import chess.ChessGame;
 import com.google.gson.Gson;
 import model.GameData;
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 import service.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -35,7 +38,23 @@ public class SQLGameDAO {
     }
 
     public ListGamesResult getListOfGames() throws DataAccessException{
-        return null;
+        ArrayList<GameData> gamesToList = new ArrayList<>();
+
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT game_id, white_username, black_username, game_name FROM game_table";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+
+                        gamesToList.add(new GameData(rs.getInt("game_id"), rs.getString("white_username"), rs.getString("black_username"), rs.getString("game_name"), null));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException (String.format("Unable to read data: %s", e.getMessage()));
+        }
+        ListGamesResult listGamesResult = new ListGamesResult(null, gamesToList);
+        return listGamesResult;
     }
 
     public CreateGameResult createGame(CreateGameRequest createGameRequest) throws DataAccessException {
@@ -67,5 +86,11 @@ public class SQLGameDAO {
 
     public JoinGameResult joinGame(JoinGameRequest joinGameRequest, String username) throws DataAccessException {
         return null;
+    }
+
+    private GameData readGame(ResultSet rs) throws SQLException {
+        String chessGameJson = rs.getString("game");
+        ChessGame chessGame = new Gson().fromJson(chessGameJson, ChessGame.class);
+        return new GameData(rs.getInt("game_id"), rs.getString("white_username"), rs.getString("black_username"), rs.getString("game_name"), chessGame);
     }
 }
