da758aeda0036e11806b225c1ed3ae0955d1ea1c
w2boy <wesleyt.wardle@gmail.com>
2024-06-12 20:03:16 -0600

    Commit before removing cache.

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -2,9 +2,8 @@ package websocket.commands;
 
 public class ConnectCommand extends UserGameCommand {
 
-    CommandType type = CommandType.CONNECT;
-
     public ConnectCommand(String authToken, int gameID){
         super(authToken, gameID);
+        commandType = CommandType.CONNECT;
     }
 }
